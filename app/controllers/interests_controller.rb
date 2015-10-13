c60700d5b7955b441ef3ccd81ba67d8f1a8871a9
Sanam Akbar <akbar.sanam@gmail.com>
2015-10-13 09:26:45 -0400

    created authentication, login, sign up

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,2 +1,23 @@
 class InterestsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @interest = Interest.new(interest_params)
+    @interest.user_id = current_user.id
+    if @interest.valid?
+      @interest.save
+
+      redirect_to root_path
+    else
+      redirect_to root_path, alert: "Ooops"
+    end
+  end
+
+  private
+
+#Never trust parameters
+  def interest_params
+    params.require(:interest).permit(:level, :comment, :topic_id)
+  end
+end
 end
